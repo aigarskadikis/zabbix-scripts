6c791b2f8803b7f8418405a1a1dfc9a2e5856e67
catonrug <aigars.kadikis@gmail.com>
2018-04-09 11:34:26 +0300

    next

--- rebuild-curl-for-centos6.sh
+++ rebuild-curl-for-centos6.sh
@@ -4,24 +4,22 @@
 yum install wget -y
 
 #installd development 
-yum groupinstall 'Development tools'
+yum groupinstall 'Development tools' -y
 
 #additional libraries
-yum install krb5-devel libidn-devel libssh2-devel nss-devel openldap-devel stunnel zlib-devel perl-Time-HiRes valgrind
+yum install krb5-devel libidn-devel libssh2-devel nss-devel openldap-devel stunnel zlib-devel perl-Time-HiRes valgrind -y
 
-@
-useradd builduser
+useradd builduser --base-dir /home --shell /bin/bash
 
+su - builduser
+
+mkdir -p ~/rpmbuild/{BUILD,RPMS,SOURCES,SPECS,SRPMS}
+
+echo "%_topdir /home/builduser/rpmbuild" > ~/.rpmmacros
 
 #download curl source
 wget http://vault.centos.org/7.4.1708/updates/Source/SPackages/curl-7.29.0-42.el7_4.1.src.rpm
 
-
-yum install mock -y
-groupadd mockbuild
-useradd -g mockbuild -s /sbin/nologin mockbuild
-
-#rebuil curl
 rpmbuild --rebuild curl-7.29.0-42.el7_4.1.src.rpm
 
 #go to dir wher new curl is having a good time
@@ -35,13 +33,4 @@ yum install http://repo.zabbix.com/zabbix/3.4/rhel/6/x86_64/zabbix-release-3.4-1
 
 #install developement packages just to satisfy zabbix compilation
 yum install mysql-devel postgresql-devel net-snmp-devel gnutls-devel sqlite-devel unixODBC-devel OpenIPMI-devel java-devel libxml2-devel iksemel-devel
-
-
-
-
-
-
-rpmbuild --rebuild
-
-
 
