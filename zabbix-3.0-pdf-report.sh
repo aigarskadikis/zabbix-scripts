603a11fc208e347591308e267195dff119a737b4
catonrug <aigars.kadikis@gmail.com>
2018-04-27 09:06:05 +0300

    well comented

--- zabbix-3.0-pdf-report.sh
+++ zabbix-3.0-pdf-report.sh
@@ -21,11 +21,21 @@ tar -xvzf ~/zabbix-pdf-report-0.9.4.tgz -C /usr/share/zabbix
 #move to the frontend dir
 cd /usr/share/zabbix/report
 
+#take a note what is direct ip address of server
+ip a
+#this will show ip address like 10.0.2.15
+
 #enter credentials
 vim config.inc.php
-#$z_server       = 'http://192.168.3.246/zabbix/';
+#$z_server       = 'http://10.0.2.15/zabbix/';
 #$z_user         = 'Admin';
 #$z_pass         = 'zabbix';
 
-#go to
-http://192.168.3.246/zabbix/report
+#based on port forwarding in VirtualBox
+#Protocol: TCP, Host IP: 127.0.0.1, Host Port: 8030, Guest IP: 10.0.2.15, Guest Port:80
+#https://1.bp.blogspot.com/-F-z-K3xfN2o/WuK9OSiTr5I/AAAAAAAAaFI/VHn-R5YByEscoW1H3aVri1sEsbbUg9w2gCLcBGAs/s1600/port-forwarding-virtualbox-zabbix.png
+#you need to go to address
+#http://localhost:8030/zabbix/report
+
+#related
+#https://catonrug.blogspot.com/2018/04/install-zabbix-dynamic-pdf-report.html
