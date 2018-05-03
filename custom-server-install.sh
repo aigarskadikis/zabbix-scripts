1de61b9923e801d6b8c8044993c8a60acccd1e33
catonrug <aigars.kadikis@gmail.com>
2018-05-03 13:18:14 +0300

    tested, works well

--- custom-server-install.sh
+++ custom-server-install.sh
@@ -7,9 +7,19 @@ yum update -y
 yum install yum-utils -y
 yum-config-manager --enable rhel-7-server-optional-rpms
 
+#open 80 and 443 into firewall
+systemctl enable firewalld
+systemctl start firewalld
+
+firewall-cmd --permanent --add-service=http
+firewall-cmd --permanent --add-service=https
+firewall-cmd --add-port=162/udp --permanent
+firewall-cmd --add-port=3000/tcp --permanent #for grafana reporting server https://www.digitalocean.com/community/tutorials/how-to-install-and-configure-grafana-to-plot-beautiful-graphs-from-zabbix-on-centos-7
+firewall-cmd --reload
+
 #install SELinux debuging utils
 #yum install policycoreutils-python -y
-
+setenforce 0
 
 #install mariadb (mysql database engine for CentOS 7)
 yum install mariadb-server -y
@@ -23,9 +33,6 @@ echo $?
 
 #show existing databases
 mysql -h localhost -uroot -p5sRj4GXspvDKsBXW -P 3306 -s <<< 'show databases;' | grep zabbix
-if [ $? -eq 0 ]; then
-echo zabbix database already exist. cannot continue
-else
 #create zabbix database
 mysql -h localhost -uroot -p5sRj4GXspvDKsBXW -P 3306 -s <<< 'create database zabbix character set utf8 collate utf8_bin;'
 
@@ -41,7 +48,7 @@ mysql -h localhost -uroot -p5sRj4GXspvDKsBXW -P 3306 -s <<< 'show databases;' | 
 #enable to start MySQL automatically at next boot
 systemctl enable mariadb
 
-rpm -i zabbix-server-mysql-*
+rpm -iv zabbix-server-mysql-*
 #error: Failed dependencies:
 #        fping is needed by zabbix-server-mysql-3.4.8-0.0.1r78441.el7.x86_64
 #        libOpenIPMI.so.0()(64bit) is needed by zabbix-server-mysql-3.4.8-0.0.1r78441.el7.x86_64
@@ -52,6 +59,7 @@ rpm -i zabbix-server-mysql-*
 #        libodbc.so.2()(64bit) is needed by zabbix-server-mysql-3.4.8-0.0.1r78441.el7.x86_64
 
 rpm -ivh https://repo.zabbix.com/non-supported/rhel/7/x86_64/fping-3.10-1.el7.x86_64.rpm
+yum install OpenIPMI-libs libevent net-snmp-libs unixODBC -y
 
 ## rpm -ivh https://repo.zabbix.com/non-supported/rhel/7/x86_64/iksemel-devel-1.4-2.el7.centos.x86_64.rpm
 #error: Failed dependencies:
@@ -81,4 +89,55 @@ rpm -iv zabbix-server-mysql-*
 #create zabbix database structure
 ls -l /usr/share/doc/zabbix-server-mysql*/
 zcat /usr/share/doc/zabbix-server-mysql*/create.sql.gz | mysql -uzabbix -pTaL2gPU5U9FcCU2u zabbix
+
+server=/etc/zabbix/zabbix_server.conf
+#check if there is existing password line in config
+grep "DBPassword=" $server
+#change the password
+sed -i "s/^.*DBPassword=.*$/DBPassword=TaL2gPU5U9FcCU2u/g" $server
+grep -v "^$\|^#" $server
+
+#start
+systemctl start zabbix-server
+echo $?
+
+cat /var/log/zabbix/zabbix_server.log
+
+#enable after reboot
+systemctl enable zabbix-server
+
+#install web server
+yum install httpd -y
+
+#install front end
+rpm -iv zabbix-web-3.*
+#error: Failed dependencies:
+#        dejavu-sans-fonts is needed by zabbix-web
+#        php >= 5.4 is needed by zabbix-web
+#        php-bcmath is needed by zabbix-web
+#        php-gd is needed by zabbix-web
+#        php-ldap is needed by zabbix-web
+#        php-mbstring is needed by zabbix-web
+#        php-xml is needed by zabbix-web
+#        zabbix-web-database = 3.4.8-0.0.1r78441.el7 is needed by zabbix-web
+
+yum install dejavu-sans-fonts php php-bcmath php-gd php-ldap php-mbstring php-xml php-mysql -y
+
+rpm -iv zabbix-web-mysql-* zabbix-web-3.*
+
+#set up timezone
+sed -i "s/^.*php_value date.timezone .*$/php_value date.timezone Europe\/Riga/" /etc/httpd/conf.d/zabbix.conf
+
+systemctl restart httpd
+systemctl status httpd
+
+rpm -iv zabbix-agent-*
+systemctl start zabbix-agent
+echo $?
+systemctl enable zabbix-agent
+
+rpm -iv zabbix-get-*
+rpm -iv zabbix-sender-*
+
+
 
