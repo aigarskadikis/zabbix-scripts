75e7589effeec40378e01305cda1c2dddad2ea85
catonrug <aigars.kadikis@gmail.com>
2018-06-04 11:47:07 +0300

    next

--- MySQL/backupt-restore-history.sh
+++ MySQL/backupt-restore-history.sh
@@ -1,8 +1,8 @@
 
 # agenda:
 # migrate CentOS 7 default (latest) MySQL 5.5 to MariaDB 12.3
-# create daily partitioning from 2018-01-01 to this day for tables: history_log, history_str, history_text, history_uint, history
-# enable automaticall partitioning in future for these 5 tables by using native MariaDB procedures listed
+# create daily partitioning from 2018-01-01 to this day for tables: history_log, history_str, history_text, history_uint, history, trends, trends_uint
+# enable automaticall partitioning in future for these 7 tables by using native MariaDB procedures listed
 #
 # why?:
 # http://www.vertabelo.com/blog/technical-articles/everything-you-need-to-know-about-mysql-partitions
@@ -10,8 +10,8 @@
 
 # steps to produce:
 # * stop zabbix-server, zabbix-agent. disable services at startup
-# * backup everything except tables: history, history_uint, history_text, history_str, history_log, trends, trends_uint. https://catonrug.blogspot.com/2018/04/backup-zabbix-database-without-history-mysql.html
 # * list/save show create table for all 7 biggest tables
+# * backup everything except tables: history, history_uint, history_text, history_str, history_log, trends, trends_uint. https://catonrug.blogspot.com/2018/04/backup-zabbix-database-without-history-mysql.html
 # * backup all 7 biggest tables separately
 # * remove MySQL 5.5, yum remove mariadb mariadb-server && rm -rf /etc/my.cnf && rm -rf /var/lib/mysql
 # * clean yum cache, yum clean all && rm -rf /var/cache/yum
@@ -30,50 +30,104 @@ systemctl stop {zabbix-server,zabbix-agent}
 systemctl status {zabbix-server,zabbix-agent}
 systemctl disable {zabbix-server,zabbix-agent}
 
-# * backup everything except tables: history, history_uint, history_text, history_str, history_log
-
+# * list/save show create table for all 7 biggest tables
 mysql -u$(grep "^DBUser" /etc/zabbix/zabbix_server.conf|sed "s/^.*=//") -p$(grep "^DBPassword" /etc/zabbix/zabbix_server.conf|sed "s/^.*=//") #authorize in mysql
 show create table zabbix.history\G
 show create table zabbix.history_uint\G
 show create table zabbix.history_str\G
 show create table zabbix.history_text\G
 show create table zabbix.history_log\G
+show create table zabbix.trends\G
+show create table zabbix.trends_uint\G
 
+#this will produce. I have to manuallu add ; at the end
+CREATE TABLE `history` (
+  `itemid` bigint(20) unsigned NOT NULL,
+  `clock` int(11) NOT NULL DEFAULT '0',
+  `value` double(16,4) NOT NULL DEFAULT '0.0000',
+  `ns` int(11) NOT NULL DEFAULT '0',
+  KEY `history_1` (`itemid`,`clock`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+CREATE TABLE `history_uint` (
+  `itemid` bigint(20) unsigned NOT NULL,
+  `clock` int(11) NOT NULL DEFAULT '0',
+  `value` bigint(20) unsigned NOT NULL DEFAULT '0',
+  `ns` int(11) NOT NULL DEFAULT '0',
+  KEY `history_uint_1` (`itemid`,`clock`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+CREATE TABLE `history_str` (
+  `itemid` bigint(20) unsigned NOT NULL,
+  `clock` int(11) NOT NULL DEFAULT '0',
+  `value` varchar(255) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `ns` int(11) NOT NULL DEFAULT '0',
+  KEY `history_str_1` (`itemid`,`clock`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+CREATE TABLE `history_text` (
+  `itemid` bigint(20) unsigned NOT NULL,
+  `clock` int(11) NOT NULL DEFAULT '0',
+  `value` text COLLATE utf8_bin NOT NULL,
+  `ns` int(11) NOT NULL DEFAULT '0',
+  KEY `history_text_1` (`itemid`,`clock`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+CREATE TABLE `history_log` (
+  `itemid` bigint(20) unsigned NOT NULL,
+  `clock` int(11) NOT NULL DEFAULT '0',
+  `timestamp` int(11) NOT NULL DEFAULT '0',
+  `source` varchar(64) COLLATE utf8_bin NOT NULL DEFAULT '',
+  `severity` int(11) NOT NULL DEFAULT '0',
+  `value` text COLLATE utf8_bin NOT NULL,
+  `logeventid` int(11) NOT NULL DEFAULT '0',
+  `ns` int(11) NOT NULL DEFAULT '0',
+  KEY `history_log_1` (`itemid`,`clock`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+CREATE TABLE `trends` (
+  `itemid` bigint(20) unsigned NOT NULL,
+  `clock` int(11) NOT NULL DEFAULT '0',
+  `num` int(11) NOT NULL DEFAULT '0',
+  `value_min` double(16,4) NOT NULL DEFAULT '0.0000',
+  `value_avg` double(16,4) NOT NULL DEFAULT '0.0000',
+  `value_max` double(16,4) NOT NULL DEFAULT '0.0000',
+  PRIMARY KEY (`itemid`,`clock`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+CREATE TABLE `trends_uint` (
+  `itemid` bigint(20) unsigned NOT NULL,
+  `clock` int(11) NOT NULL DEFAULT '0',
+  `num` int(11) NOT NULL DEFAULT '0',
+  `value_min` bigint(20) unsigned NOT NULL DEFAULT '0',
+  `value_avg` bigint(20) unsigned NOT NULL DEFAULT '0',
+  `value_max` bigint(20) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`itemid`,`clock`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
-#backup zabbix all tables execpt the 5 biggest tables
+# hit ctrl + c to exit the client
+
+# install prerequisite for compression
 yum install bzip2 -y
+
+# set timestamp for whole process
 time=$(date +%Y%m%d%H%M)
-sudo mysqldump -uroot -p5sRj4GXspvDKsBXW --flush-logs --single-transaction --create-options --ignore-table=zabbix.history_log --ignore-table=zabbix.history_str --ignore-table=zabbix.history_text --ignore-table=zabbix.history_uint --ignore-table=zabbix.history zabbix | bzip2 -9 > /root/$time.bz2
-cd /root
-echo uploading file..
-./uploader.py uploader.cfg /root/$time.bz2
 
-# backup only tables: 
-# history_log
-# history_str
-# history_text
-# history_uint
-# history
+# * backup everything except tables: history, history_uint, history_text, history_str, history_log, trends, trends_uint
+sudo mysqldump -uroot -p5sRj4GXspvDKsBXW --flush-logs --single-transaction --create-options --ignore-table=zabbix.history_log --ignore-table=zabbix.history_str --ignore-table=zabbix.history_text --ignore-table=zabbix.history_uint --ignore-table=zabbix.history --ignore-table=zabbix.trends --ignore-table=zabbix.trends_uint zabbix | bzip2 -9 > /root/$time.without.history.trends.bz2
 
-time=$(date +%Y%m%d%H%M)
-sudo mysqldump -uroot -p5sRj4GXspvDKsBXW --flush-logs --single-transaction --no-create-db --no-create-info zabbix history_log | bzip2 -9 > /root/history_log.$time.bz2
-sudo mysqldump -uroot -p5sRj4GXspvDKsBXW --flush-logs --single-transaction --no-create-db --no-create-info zabbix history_str | bzip2 -9 > /root/history_str.$time.bz2
-sudo mysqldump -uroot -p5sRj4GXspvDKsBXW --flush-logs --single-transaction --no-create-db --no-create-info zabbix history_text | bzip2 -9 > /root/history_text.$time.bz2
-sudo mysqldump -uroot -p5sRj4GXspvDKsBXW --flush-logs --single-transaction --no-create-db --no-create-info zabbix history_uint | bzip2 -9 > /root/history_uint.$time.bz2
-sudo mysqldump -uroot -p5sRj4GXspvDKsBXW --flush-logs --single-transaction --no-create-db --no-create-info zabbix history | bzip2 -9 > /root/history.$time.bz2
-ls -lah /root/history*
-./uploader.py uploader.cfg /root/history_log.$time.bz2
-./uploader.py uploader.cfg /root/history_str.$time.bz2
-./uploader.py uploader.cfg /root/history_text.$time.bz2
-./uploader.py uploader.cfg /root/history_uint.$time.bz2
-./uploader.py uploader.cfg /root/history.$time.bz2
+# backup all 7 biggest tablas separatelly.
 
+sudo mysqldump -uroot -p5sRj4GXspvDKsBXW --flush-logs --single-transaction --no-create-db --no-create-info zabbix history_log | bzip2 -9 > /root/$time.history_log.bz2
+sudo mysqldump -uroot -p5sRj4GXspvDKsBXW --flush-logs --single-transaction --no-create-db --no-create-info zabbix history_str | bzip2 -9 > /root/$time.history_str.bz2
+sudo mysqldump -uroot -p5sRj4GXspvDKsBXW --flush-logs --single-transaction --no-create-db --no-create-info zabbix history_text | bzip2 -9 > /root/$time.history_text.bz2
+sudo mysqldump -uroot -p5sRj4GXspvDKsBXW --flush-logs --single-transaction --no-create-db --no-create-info zabbix history_uint | bzip2 -9 > /root/$time.history_uint.bz2
+sudo mysqldump -uroot -p5sRj4GXspvDKsBXW --flush-logs --single-transaction --no-create-db --no-create-info zabbix history | bzip2 -9 > /root/$time.history.bz2
+sudo mysqldump -uroot -p5sRj4GXspvDKsBXW --flush-logs --single-transaction --no-create-db --no-create-info zabbix trends | bzip2 -9 > /root/$time.trends.bz2
+sudo mysqldump -uroot -p5sRj4GXspvDKsBXW --flush-logs --single-transaction --no-create-db --no-create-info zabbix trends_uint | bzip2 -9 > /root/$time.trends_uint.bz2
 
+#list all backup files - there shoud be 8 files and every file should be bigger than 0
+ls -lah /root/$time*
 
 yum remove mariadb mariadb-server
 rm -rf /var/lib/mysql
+# or if the space lets to do cold backup
+mv /var/lib/mysql ~
 mv /etc/my.cnf ~
-
 
 #create MariaDB repo
 #https://downloads.mariadb.org/mariadb/repositories/#mirror=nluug&distro=CentOS&distro_release=centos7-amd64--centos7&version=10.3
