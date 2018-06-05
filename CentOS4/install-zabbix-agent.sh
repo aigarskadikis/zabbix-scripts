501a9b2ca9d3f42605c8e997824447587df1e22c
catonrug <aigars.kadikis@gmail.com>
2018-06-05 14:58:15 +0300

    perfect

--- CentOS4/install-zabbix-agent.sh
+++ CentOS4/install-zabbix-agent.sh
@@ -1,48 +1,53 @@
-#http://vault.centos.org/4.9/isos/i386/CentOS-4.8-i386-binDVD.torrent
+#!/bin/bash
+# this instruction is supposed to work with
+# http://vault.centos.org/4.9/isos/i386/CentOS-4.8-i386-binDVD.torrent
+# while going through installation wizard please also select to install 'gcc' and 'make' packages
 
-#getting CentOS 4 repo to work
-cp /etc/yum.repos.d/CentOS-Base.repo ~ #backup original repo
-curl http://vault.centos.org/4.8/CentOS-Base.repo > /etc/yum.repos.d/CentOS-Base.repo #overwrite original
+#backup original repository
+cp /etc/yum.repos.d/CentOS-Base.repo ~
+#overwrite original repository
+curl http://vault.centos.org/4.8/CentOS-Base.repo > /etc/yum.repos.d/CentOS-Base.repo
 
-yum update
+#update system
+yum -y update
+
+#download and extract Zabbix 3.0 agent
 cd
-wget --no-check-certificate http://downloads.sourceforge.net/project/zabbix/ZABBIX%20Latest%20Stable/3.0.17/zabbix-3.0.17.tar.gz
+wget --no-check-certificate http://downloads.sourceforge.net/project/zabbix/ZABBIX%20Latest%20Stable/3.0.18/zabbix-3.0.18.tar.gz
 tar -vzxf zabbix-*.tar.gz -C .
+#move to the source
 cd zabbix-*
+#customize the installation
 ./configure --enable-agent
-
+#compile
 time make install
 
-grep -v "^$\|^#" /usr/local/etc/zabbix_agentd.conf
-
+#point to the Zabbix server
 sed -i "s/^Server=.*/Server=10.0.2.5/" /usr/local/etc/zabbix_agentd.conf
 sed -i "s/^ServerActive=.*/ServerActive=10.0.2.5/" /usr/local/etc/zabbix_agentd.conf
+#set hostname
 sed -i "s/^Hostname=.*/Hostname=CentOS4/" /usr/local/etc/zabbix_agentd.conf
-
+#output all custom settings
 grep -v "^$\|^#" /usr/local/etc/zabbix_agentd.conf
 
+#create group 'zabbix', create user 'zabbix' and assign it to group 'zabbix'
 groupadd zabbix
 useradd -g zabbix zabbix
 
-#take init script from old version
-cd
-wget --no-check-certificate http://downloads.sourceforge.net/project/zabbix/ZABBIX%20Latest%20Stable/1.8.20/zabbix-1.8.20.tar.gz
-tar -vzxf zabbix-1.8.20.tar.gz -C .
-cat zabbix-1.8.20/misc/init.d/redhat/zabbix_agentd_ctl > /etc/init.d/zabbix-agent
-#set the base and pid according to system
-# base zabbix dir
-#BASEDIR=/usr/local
-# pid file (as of 1.0 beta 10)
-#PIDFILE=/tmp/zabbix_agentd.pid
-sed -i "s/^BASEDIR=.*/BASEDIR=\/usr\/local/" /etc/init.d/zabbix-agent
-sed -i "s/^PIDFILE=.*/PIDFILE=\/tmp\/zabbix_agentd.pid/" /etc/init.d/zabbix-agent
-chmod +x /etc/init.d/zabbix-agent
+#install startup script
+cp ~/zabbix-*/misc/init.d/fedora/core/zabbix_agentd /etc/init.d
 
+#start the agent
+/etc/init.d/zabbix_agentd start
+#check if agent is running
+/etc/init.d/zabbix_agentd status
 
+sleep 1
+#look at the log
+cat /tmp/zabbix_agentd.log
 
-wget --no-check-certificate "https://ayera.dl.sourceforge.net/project/zabbix/ZABBIX%20Latest%20Stable/1.0/zabbix-1.0.tar.gz"
-tar -vzxf zabbix-1.0.tar.gz -C .
+#enable zabbix agent to lauch at system startup
+chkconfig zabbix_agentd on
 
-wget "http://master.dl.sourceforge.net/project/zabbix/ZABBIX%20Latest%20Stable/1.1.1/zabbix-1.1.1.tar.gz"
-tar -vzxf zabbix-1.1.1.tar.gz -C .
-
+#reboot the system to see if the agent starts automatically at startup
+reboot
