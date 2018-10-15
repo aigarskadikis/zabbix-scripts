2eee920f9c424d3b2dfb5a2df39e1b9c034e2c23
catonrug <aigars.kadikis@gmail.com>
2018-10-15 17:14:05 +0300

    next

--- Oracle-XE/oracle-at-backend.sh
+++ Oracle-XE/oracle-at-backend.sh
@@ -1,6 +1,12 @@
 # this instruction shows how to setup oracle XE on CentOS 7.5 and install zabbix server with oracle database at the backend.
+# install vagrant SSH key for future passwordless root access
+sudo su
+echo "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA6NF8iallvQVp22WDkTkyrtvp9eWW6A8YVr+kz4TjGYe7gHzIw+niNltGEFHzD8+v1I2YJ6oXevct1YeS0o9HZyN1Q9qgCgzUFtdOKLv6IedplqoPkcmF0aYet2PkEDo3MlTBckFXPITAMzF8dJSIFo9D8HfdOV0IAdx4O7PtixWKn5y2hMNG0zQPyUecp4pzC6kivAIhyfHilFR61RGL+GPXQ2MWZWFYbAGjyiYJnAmCP3NOTd0jMZEnDkbUvxhMmBYSdETk1rRgm+R4LOzFUGaHqHDLKLX+FIPKcF96hrucXzcWyLbIbEgE98OHlnVYCzRdK8jlqm8tehUc9c9WhQ== imported-openssh-key" > ~/.ssh/authorized_keys
+chmod -R 600 ~/.ssh/authorized_keys
 
-yum -y install gcc gcc-c++ make gnutls-devel libxml2-devel unixODBC-devel net-snmp-devel libssh2-devel OpenIPMI-devel libevent-devel libcurl-devel openldap-devel bc net-tools vim unzip
+
+
+yum -y install gcc gcc-c++ make gnutls-devel libxml2-devel unixODBC-devel net-snmp-devel libssh2-devel OpenIPMI-devel libevent-devel libcurl-devel openldap-devel bc net-tools vim unzip mlocate
 rpm -i https://repo.zabbix.com/non-supported/rhel/7/x86_64/iksemel-1.4-2.el7.centos.x86_64.rpm
 rpm -i https://repo.zabbix.com/non-supported/rhel/7/x86_64/iksemel-devel-1.4-2.el7.centos.x86_64.rpm
 
@@ -12,10 +18,6 @@ rpm -i https://repo.zabbix.com/non-supported/rhel/7/x86_64/iksemel-devel-1.4-2.e
 # http://download.oracle.com/otn/linux/instantclient/11204/oracle-instantclient11.2-odbc-11.2.0.4.0-1.x86_64.rpm
 # http://download.oracle.com/otn/linux/instantclient/11204/oracle-instantclient11.2-tools-11.2.0.4.0-1.x86_64.rpm
 
-# install vagrant SSH key for future passwordless root access
-sudo su
-echo "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA6NF8iallvQVp22WDkTkyrtvp9eWW6A8YVr+kz4TjGYe7gHzIw+niNltGEFHzD8+v1I2YJ6oXevct1YeS0o9HZyN1Q9qgCgzUFtdOKLv6IedplqoPkcmF0aYet2PkEDo3MlTBckFXPITAMzF8dJSIFo9D8HfdOV0IAdx4O7PtixWKn5y2hMNG0zQPyUecp4pzC6kivAIhyfHilFR61RGL+GPXQ2MWZWFYbAGjyiYJnAmCP3NOTd0jMZEnDkbUvxhMmBYSdETk1rRgm+R4LOzFUGaHqHDLKLX+FIPKcF96hrucXzcWyLbIbEgE98OHlnVYCzRdK8jlqm8tehUc9c9WhQ== imported-openssh-key" > ~/.ssh/authorized_keys
-chmod -R 600 ~/.ssh/authorized_keys
 
 sysctl kernel.shmmax # check shmmax. must be 4294967295
 echo "kernel.shmmax = 4294967295" >> /etc/sysctl.conf # install shmmax=4294967295 globaly at the next boot
@@ -33,12 +35,12 @@ cd ~/Disk1
 rpm -i oracle-xe-11.2.0-1.0.x86_64.rpm
 
 time /etc/init.d/oracle-xe configure
-
 # Lets set database password '5sRj4GXspvDKsBXW' for SYS and SYSTEM account
 
 su - oracle
 echo $ORACLE_HOME
 echo ". /u01/app/oracle/product/11.2.0/xe/bin/oracle_env.sh"> ~/.bash_profile
+echo "export LD_LIBRARY_PATH=\$ORACLE_HOME/lib">> ~/.bash_profile
 exit
 su - oracle
 echo $ORACLE_HOME
@@ -111,21 +113,6 @@ SET DEFINE OFF
 
 
 @/u01/app/oracle/schema.sql
-# stop here if you setting up proxy
-
-### for images copy image files to coresponding dir with oracle user read permissions
-@/u01/app/oracle/images.sql
-
-# BEGIN
-# *
-# ERROR at line 1:
-# ORA-22288: file or LOB operation FILEOPEN failed
-# No such file or directory
-# ORA-06512: at "SYS.DBMS_LOB", line 805
-# ORA-06512: at "ZABBIX.LOAD_IMAGE", line 8
-# ORA-06512: at line 2
-
-@/u01/app/oracle/data.sql
 
 exit
 
@@ -138,6 +125,8 @@ curl -L "http://downloads.sourceforge.net/project/zabbix/ZABBIX%20Latest%20Stabl
 tar -vzxf zabbix-$v.tar.gz -C .
 cd zabbix-$v
 
+echo ". /u01/app/oracle/product/11.2.0/xe/bin/oracle_env.sh">> ~/.bash_profile
+echo "export LD_LIBRARY_PATH=\$ORACLE_HOME/lib">> ~/.bash_profile
 
 export ORACLE_HOME=/u01/app/oracle/product/11.2.0/xe
 export LD_LIBRARY_PATH=$ORACLE_HOME/lib
@@ -152,71 +141,116 @@ time make # compile
 make install # install
 
 cat /u01/app/oracle/product/11.2.0/xe/network/admin/tnsnames.ora
-vim /etc/zabbix/zabbix_proxy.conf
-# Hostname=oracle-xe
-# LogFile=/tmp/zabbix_proxy.log
-# DBName=XE
-# DBUser=zabbix
-# DBPassword=zabbix
-# DBPort=1521
-# Timeout=4
-# LogSlowQueries=3000
+grep -v "^$\|#" /etc/zabbix/zabbix_proxy.conf
+Server=ec2-35-166-97-138.us-west-2.compute.amazonaws.com
+Hostname=oracle-xe
+LogFile=/tmp/zabbix_proxy.log
+DBName=XE
+DBUser=zabbix
+DBPassword=zabbix
+DBPort=1521
+Timeout=4
+LogSlowQueries=3000
+
+cp /etc/zabbix/{zabbix_proxy.conf,original.zabbix_proxy.conf}
+cat >/etc/zabbix/zabbix_proxy.conf<< EOL
+Server=ec2-35-166-97-138.us-west-2.compute.amazonaws.com
+Hostname=oracle-xe
+LogFile=/tmp/zabbix_proxy.log
+DBName=XE
+DBUser=zabbix
+DBPassword=zabbix
+DBPort=1521
+Timeout=4
+LogSlowQueries=3000
+EOL
+
 
 # test if proxy is working
 su - zabbix
 # zabbix_proxy: error while loading shared libraries: libclntsh.so.11.1: cannot open shared object file: No such file or directory
 echo ". /u01/app/oracle/product/11.2.0/xe/bin/oracle_env.sh">> ~/.bash_profile
 echo "export LD_LIBRARY_PATH=\$ORACLE_HOME/lib">> ~/.bash_profile
+export ORACLE_HOME=/u01/app/oracle/product/11.2.0/xe
+export LD_LIBRARY_PATH=$ORACLE_HOME/lib
 
+exit
 zabbix_proxy -f
 
 cd 
 
+# set up ODBC connection
+find / -name odbc_update_ini.sh
+
+locate libodbcinst
+cd /usr/lib64
+ln -s libodbcinst.so.2 libodbcinst.so.1
+chmod 755 /usr/lib/oracle/11.2/client64/lib/lib*
+ldd /usr/lib/oracle/11.2/client64/lib/libsqora.so.11.1
+isql
+
+cat >/etc/odbc.ini<< EOL
+Application Attributes = T
+Attributes = W
+BatchAutocommitMode = IfAllSuccessful
+BindAsFLOAT = F
+CloseCursor = F
+DisableDPM = F
+DisableMTS = T
+Driver = Oracle 11g ODBC driver
+DSN = XE
+EXECSchemaOpt =
+EXECSyntax = T
+Failover = T
+FailoverDelay = 10
+FailoverRetryCount = 10
+FetchBufferSize = 64000
+ForceWCHAR = F
+Lobs = T
+Longs = T
+MaxLargeData = 0
+MetadataIdDefault = F
+QueryTimeout = T
+ResultSets = T
+ServerName = oracle-be.pro
+SQLGetData extensions = F
+Translation DLL =
+Translation Option = 0
+DisableRULEHint = T
+UserID = zabbix
+Password = zabbix
+StatementCache=F
+CacheBufferSize=20
+UseOCIDescribeAny=F
+MaxTokenSize=8192
+EOL
+
+isql -v ODBC
+
+./odbc_update_ini.sh / 
+
+/usr/lib/oracle/11.2/client64/lib/libsqora.so.11.1
+
+# http://docs.adaptivecomputing.com/9-1-0/MWS/Content/topics/moabWorkloadManager/topics/databases/oracle.html
+
 # install other 
-rpm -i oracle-instantclient11.2-basic-11.2.0.4.0-1.x86_64.rpm # this is needed for httpd client to be able to connect
-rpm -i oracle-instantclient11.2-devel-11.2.0.4.0-1.x86_64.rpm
-rpm -i oracle-instantclient11.2-jdbc-11.2.0.4.0-1.x86_64.rpm
+rpm -i oracle-instantclient11.2-basic-11.2.0.4.0-1.x86_64.rpm # required for odbc. this is needed for httpd client to be able to connect. 
 rpm -i oracle-instantclient11.2-odbc-11.2.0.4.0-1.x86_64.rpm # odbc connector. needed to use direct SQL statements
+
+rpm -i oracle-instantclient11.2-devel-11.2.0.4.0-1.x86_64.rpm
 rpm -i oracle-instantclient11.2-tools-11.2.0.4.0-1.x86_64.rpm
+rpm -i oracle-instantclient11.2-jdbc-11.2.0.4.0-1.x86_64.rpm
+
 
 # zabbix_server: error while loading shared libraries: libclntsh.so.11.1: cannot open shared object file: No such file or directory
 
-
-# bash: pecl: command not found
-yum -y install php-pear
-
-# WARNING: channel "pecl.php.net" has updated its protocols, use "pecl channel-update pecl.php.net" to update
-# pecl/oci8 requires PHP (version >= 7.0.0), installed version is 5.4.16
-
-yum install epel-release
-yum install http://rpms.remirepo.net/enterprise/remi-release-7.rpm
-yum-config-manager --enable remi-php70
-yum install php php-mcrypt php-cli php-gd php-curl php-mysql php-ldap php-zip php-fileinfo
-php -v
-
-# PHP Parse error:  syntax error, unexpected 'new' (T_NEW) in /usr/share/pear/PEAR/Frontend.php on line 91
-yum update -y
-
-pecl install oci8
-pecl channel-update pecl.php.net
-# Can't find PHP headers in /usr/include/php
-yum -y install php-devel
-
-# /var/tmp/oci8/php_oci8_int.h:46:29: fatal error: oci8_dtrace_gen.h: No such file or directory
-export PHP_DTRACE=yes
-pecl install oci8
-# '/u01/app/oracle/product/11.2.0/xe'
-
-# Build process completed successfully
-# Installing '/usr/lib64/php/modules/oci8.so'
-# install ok: channel://pecl.php.net/oci8-2.1.8
-# configuration option "php_ini" is not set to php.ini location
-# You should add "extension=oci8.so" to php.ini
+# [01000][unixODBC][Driver Manager]Can't open lib '/usr/lib/oracle/11.2/client64/lib/libsqora.so.11.1' : file not found
+# [ISQL]ERROR: Could not SQLConnect
+echo ". /u01/app/oracle/product/11.2.0/xe/bin/oracle_env.sh">> ~/.bash_profile
+echo "export LD_LIBRARY_PATH=\$ORACLE_HOME/lib">> ~/.bash_profile
 
 
-
-# 
-# https://ucblog.ru/2018/04/zabbix-%D0%BC%D0%BE%D0%BD%D0%B8%D1%82%D0%BE%D1%80%D0%B8%D0%BD%D0%B3-%D0%B1%D0%B0%D0%B7%D1%8B-%D0%B4%D0%B0%D0%BD%D0%BD%D1%8B%D1%85-oracle/
+#https://ucblog.ru/2018/04/zabbix-%D0%BC%D0%BE%D0%BD%D0%B8%D1%82%D0%BE%D1%80%D0%B8%D0%BD%D0%B3-%D0%B1%D0%B0%D0%B7%D1%8B-%D0%B4%D0%B0%D0%BD%D0%BD%D1%8B%D1%85-oracle/
 # https://www.tekstream.com/oracle-error-messages/ora-01034-oracle-not-available/
 
 # https://www.zabbix.com/documentation/4.0/manual/appendix/install/db_scripts
