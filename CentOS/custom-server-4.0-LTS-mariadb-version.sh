939b672a57659b0b17343699d2f334739e4d8346
catonrug <aigars.kadikis@gmail.com>
2018-11-13 17:55:09 +0200

    next

--- CentOS/custom-server-4.0-LTS-mariadb-version.sh
+++ CentOS/custom-server-4.0-LTS-mariadb-version.sh
@@ -264,6 +264,10 @@ mkdir -p ~/.ssh
 echo "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA6NF8iallvQVp22WDkTkyrtvp9eWW6A8YVr+kz4TjGYe7gHzIw+niNltGEFHzD8+v1I2YJ6oXevct1YeS0o9HZyN1Q9qgCgzUFtdOKLv6IedplqoPkcmF0aYet2PkEDo3MlTBckFXPITAMzF8dJSIFo9D8HfdOV0IAdx4O7PtixWKn5y2hMNG0zQPyUecp4pzC6kivAIhyfHilFR61RGL+GPXQ2MWZWFYbAGjyiYJnAmCP3NOTd0jMZEnDkbUvxhMmBYSdETk1rRgm+R4LOzFUGaHqHDLKLX+FIPKcF96hrucXzcWyLbIbEgE98OHlnVYCzRdK8jlqm8tehUc9c9WhQ== vagrant insecure public key"> ~/.ssh/authorized_keys
 chmod -R 700 ~/.ssh
 
+# MariaDB-backup
+yum -y install MariaDB-backup
+
+
 # install prerequsites for partitioning script
 yum -y install epel-release && yum makecache
 yum -y install python-yaml mysql-connector-python
