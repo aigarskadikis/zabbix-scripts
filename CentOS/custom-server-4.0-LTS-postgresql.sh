41a37dfbe640fa4dbe8251d5a99b46a5433d4ef7
catonrug <aigars.kadikis@gmail.com>
2018-12-03 13:46:25 +0200

    next

--- CentOS/custom-server-4.0-LTS-postgresql.sh
+++ CentOS/custom-server-4.0-LTS-postgresql.sh
@@ -3,68 +3,65 @@
 #this is tested and works together with fresh CentOS-7-x86_64-Minimal-1708.iso
 #cd && curl https://raw.githubusercontent.com/catonrug/zabbix-scripts/master/CentOS/custom-server-4.0-LTS-postgresql.sh > install.sh && chmod +x install.sh && time ./install.sh 4.0.2   
 
-#open 80 and 443 into firewall
-systemctl enable firewalld
-systemctl start firewalld
-
-firewall-cmd --permanent --add-service=http
-firewall-cmd --permanent --add-service=https
-firewall-cmd --permanent --add-service=mysql
-firewall-cmd --add-port=162/udp --permanent
-firewall-cmd --add-port=3000/tcp --permanent #for grafana reporting server https://www.digitalocean.com/community/tutorials/how-to-install-and-configure-grafana-to-plot-beautiful-graphs-from-zabbix-on-centos-7
-firewall-cmd --add-port=10050/tcp --permanent
-firewall-cmd --add-port=10051/tcp --permanent
-firewall-cmd --reload
-
 #update system
 yum update -y
 
 #install SELinux debuging utils
 yum -y install policycoreutils-python bzip2 nmap vim 
 
-# https://www.postgresql.org/download/linux/redhat/
-
-rpm -ivh https://download.postgresql.org/pub/repos/yum/9.5/redhat/rhel-7-x86_64/pgdg-centos95-9.5-3.noarch.rpm
-
-yum -y install postgresql95
-yum -y install postgresql95-server
+yum -y install postgresql && yum -y install postgresql-server
 
 setenforce 0
 
-/usr/pgsql-9.5/bin/postgresql95-setup initdb
-systemctl enable postgresql-9.5
-systemctl start postgresql-9.5
+/usr/bin/postgresql-setup initdb
 
+systemctl enable postgresql
+systemctl start postgresql
 
 rpm -ivh http://repo.zabbix.com/zabbix/4.0/rhel/7/x86_64/zabbix-release-4.0-1.el7.noarch.rpm
 
 yum -y install zabbix-server-pgsql zabbix-web-pgsql zabbix-agent
 
-sudo -u postgres dropdb zabbix
+# sudo -u postgres dropdb zabbix
+# sudo -u postgres dropuser zabbix
 
-sudo -u postgres dropuser zabbix
-
-
-sudo -u postgres createuser --pwprompt zabbix
+sudo -u postgres bash -c "psql -c \"CREATE USER zabbix WITH PASSWORD 'zabbix';\""
+# sudo -u postgres createuser --pwprompt zabbix
 
 sudo -u postgres createdb -O zabbix zabbix
 
 zcat /usr/share/doc/zabbix-server-pgsql*/create.sql.gz | sudo -u zabbix psql zabbix
 
-su - postgres 
+#define server conf file
+server=/etc/zabbix/zabbix_server.conf
 
-DBHost=
-DBPassword=zabbix
+grep "^DBPassword=" $server
+if [ $? -eq 0 ]; then
+sed -i "s/^DBPassword=.*/DBPassword=zabbix/" $server #modifies already customized setting
+else
+ln=$(grep -n "DBPassword=" $server | egrep -o "^[0-9]+"); ln=$((ln+1)) #calculate the the line number after default setting
+sed -i "`echo $ln`iDBPassword=zabbix" $server #adds new line
+fi
+
+grep "^DBHost=" $server
+if [ $? -eq 0 ]; then
+sed -i "s/^DBHost=.*/DBHost=/" $server #modifies already customized setting
+else
+ln=$(grep -n "DBHost=" $server | egrep -o "^[0-9]+"); ln=$((ln+1)) #calculate the the line number after default setting
+sed -i "`echo $ln`iDBHost=" $server #adds new line
+fi
 
 
-vim /etc/httpd/conf.d/zabbix.conf
-
-systemctl restart zabbix-server zabbix-agent httpd
-systemctl enable zabbix-server zabbix-agent httpd
+sed -i "s/^.*php_value date.timezone .*$/php_value date.timezone Europe\/Riga/" /etc/httpd/conf.d/zabbix.conf
 
 
+vim /var/lib/pgsql/data/pg_hba.conf
+# "local" is for Unix domain socket connections only
+local   all             all                                     md5
+# IPv4 local connections:
+host    all             all             127.0.0.1/32            md5
+# IPv6 local connections:
+host    all             all             ::1/128                 md5
 
+systemctl restart zabbix-server zabbix-agent postgresql httpd && systemctl enable zabbix-server zabbix-agent httpd
 
-
-setsebool -P httpd_can_network_connect on
-setsebool -P zabbix_can_network on
