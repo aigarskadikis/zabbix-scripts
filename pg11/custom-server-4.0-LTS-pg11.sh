101ab9477c186757592caa980541f5f7e83929a4
catonrug <aigars.kadikis@gmail.com>
2019-02-07 13:22:19 +0200

    next

--- pg11/custom-server-4.0-LTS-pg11.sh
+++ pg11/custom-server-4.0-LTS-pg11.sh
@@ -91,6 +91,52 @@ host    all             all             127.0.0.1/32            md5
 host    all             all             ::1/128                 md5
 EOF
 
+cat <<'EOF'>/etc/zabbix/web/zabbix.conf.php
+<?php
+// Zabbix GUI configuration file.
+global $DB;
+
+$DB['TYPE']     = 'POSTGRESQL';
+$DB['SERVER']   = 'localhost';
+$DB['PORT']     = '0';
+$DB['DATABASE'] = 'zabbix';
+$DB['USER']     = 'zabbix';
+$DB['PASSWORD'] = 'zabbix';
+
+// Schema name. Used for IBM DB2 and PostgreSQL.
+$DB['SCHEMA'] = '';
+
+$ZBX_SERVER      = 'localhost';
+$ZBX_SERVER_PORT = '10051';
+$ZBX_SERVER_NAME = 'pg11';
+
+$IMAGE_FORMAT_DEFAULT = IMAGE_FORMAT_PNG;
+EOF
+
+#configure zabbix to host on root
+grep "^Alias" /etc/httpd/conf.d/zabbix.conf
+if [ $? -ne 0 ]; then
+echo Alias not found in "/etc/httpd/conf.d/zabbix.conf". Something is out of order.
+else
+#replace one line:
+#Alias /zabbix /usr/share/zabbix-agent
+#with two lines
+#<VirtualHost *:80>
+#DocumentRoot /usr/share/zabbix
+sed -i "s/Alias \/zabbix \/usr\/share\/zabbix/<VirtualHost \*:80>\nDocumentRoot \/usr\/share\/zabbix/" /etc/httpd/conf.d/zabbix.conf
+
+#add to the end of the file:
+#</VirtualHost>
+grep "</VirtualHost>" /etc/httpd/conf.d/zabbix.conf
+if [ $? -eq 0 ]; then
+echo "</VirtualHost>" already exists in the file /etc/httpd/conf.d/zabbix.conf
+else
+echo "</VirtualHost>" >> /etc/httpd/conf.d/zabbix.conf
+fi
+
+sed -i "s/^/#/g" /etc/httpd/conf.d/welcome.conf
+
+
 # start everything after reboot
 systemctl restart postgresql-11 zabbix-server zabbix-agent httpd && systemctl enable zabbix-server zabbix-agent httpd
 
