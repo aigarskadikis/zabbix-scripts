3986011c3863e284c24a781c4cdf3440458cf81c
catonrug <aigars.kadikis@gmail.com>
2019-02-07 13:27:24 +0200

    next

--- pg11/custom-server-4.0-LTS-pg11.sh
+++ pg11/custom-server-4.0-LTS-pg11.sh
@@ -91,7 +91,7 @@ host    all             all             127.0.0.1/32            md5
 host    all             all             ::1/128                 md5
 EOF
 
-cat <<'EOF'>/etc/zabbix/web/zabbix.conf.php
+cat <<'EOF'> /etc/zabbix/web/zabbix.conf.php
 <?php
 // Zabbix GUI configuration file.
 global $DB;
@@ -112,29 +112,6 @@ $ZBX_SERVER_NAME = 'pg11';
 
 $IMAGE_FORMAT_DEFAULT = IMAGE_FORMAT_PNG;
 EOF
-
-#configure zabbix to host on root
-grep "^Alias" /etc/httpd/conf.d/zabbix.conf
-if [ $? -ne 0 ]; then
-echo Alias not found in "/etc/httpd/conf.d/zabbix.conf". Something is out of order.
-else
-#replace one line:
-#Alias /zabbix /usr/share/zabbix-agent
-#with two lines
-#<VirtualHost *:80>
-#DocumentRoot /usr/share/zabbix
-sed -i "s/Alias \/zabbix \/usr\/share\/zabbix/<VirtualHost \*:80>\nDocumentRoot \/usr\/share\/zabbix/" /etc/httpd/conf.d/zabbix.conf
-
-#add to the end of the file:
-#</VirtualHost>
-grep "</VirtualHost>" /etc/httpd/conf.d/zabbix.conf
-if [ $? -eq 0 ]; then
-echo "</VirtualHost>" already exists in the file /etc/httpd/conf.d/zabbix.conf
-else
-echo "</VirtualHost>" >> /etc/httpd/conf.d/zabbix.conf
-fi
-
-sed -i "s/^/#/g" /etc/httpd/conf.d/welcome.conf
 
 
 # start everything after reboot
