aaf4f30a899ba693529624b2a4f39ff6235d3f08
catonrug <aigars.kadikis@gmail.com>
2020-02-21 21:18:09 +0200

    next

--- update-inventory-field-for-all-hosts.sh
+++ update-inventory-field-for-all-hosts.sh
@@ -34,6 +34,6 @@ tr -cd "[:print:]" | \
 
 sed "s%^%{\"jsonrpc\":\"2.0\",\"method\":\"host.massupdate\",\"params\":{\"hosts\":\[%" | \
 
-sed "s%.$%\],\"inventory_mode\":1,\"inventory\":{\"url_c\":\"http:\/\/distrowatch.com\/\"}},\"auth\":\"b42c98b44f748dd82e2debf7fa2be02e\",\"id\":1}%" | \
+sed "s%.$%\],\"inventory_mode\":1,\"inventory\":{\"url_c\":\"https:\/\/zbx.catonrug.net\/\"}},\"auth\":\"b42c98b44f748dd82e2debf7fa2be02e\",\"id\":1}%" | \
 
 xargs -i -0 echo "http://127.0.0.1/api_jsonrpc.php --request POST --header 'Content-Type: application/json' -H 'cache-control: no-cache' -d '{}'" | xargs curl
