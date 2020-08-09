4ca78bc2d6569a1cadd014c3dd5d6808fcccc01c
catonrug <aigars.kadikis@gmail.com>
2020-08-09 16:10:48 +0300

    next

--- ubuntu/proxy5ubuntu20arm64.sh
+++ ubuntu/proxy5ubuntu20arm64.sh
@@ -11,7 +11,18 @@ tar -vzxf zabbix-$maj.$min.tar.gz
 
 cd zabbix-$maj.$min
 
-./configure --prefix=/etc --enable-proxy --enable-agent --enable-agent2 --with-sqlite3 --with-libcurl --with-libxml2 --with-ssh2 --with-net-snmp --with-openssl --with-unixodbc --enable-java
+./configure --enable-agent
+
+make install > /dev/shm/out.log
+
+#  --bindir=DIR            user executables [EPREFIX/bin]
+#  --sbindir=DIR           system admin executables [EPREFIX/sbin]
+#  --libexecdir=DIR        program executables [EPREFIX/libexec]
+#  --sysconfdir=DIR        read-only single-machine data [PREFIX/etc]
+
+./configure --sysconfdir=/etc/zabbix --prefix=/usr --enable-proxy --enable-agent --with-sqlite3 --with-libcurl --with-libxml2 --with-ssh2 --with-net-snmp --with-openssl --with-unixodbc 
+
+./configure --sysconfdir=/etc/zabbix --prefix=/usr --enable-proxy --enable-agent --enable-agent2 --with-sqlite3 --with-libcurl --with-libxml2 --with-ssh2 --with-net-snmp --with-openssl --with-unixodbc --enable-java
 
 # configure: error: no acceptable C compiler found in $PATH
 sudo apt -y install build-essential
@@ -45,8 +56,6 @@ sudo apt -y install golang
 
 # configure: error: Unable to find "javac" executable in path
 sudo apt -y install openjdk-8-jdk
-
-
 
 
 
