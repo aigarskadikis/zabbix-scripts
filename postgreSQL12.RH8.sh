5d6aa76b1fdbb0338c3d6cc0610bb538c4c08d8f
catonrug <aigars.kadikis@gmail.com>
2020-10-20 16:54:19 +0300

    next

--- postgreSQL12.RH8.sh
+++ postgreSQL12.RH8.sh
@@ -46,11 +46,16 @@ dnf install -y postgresql12-server timescaledb-postgresql-12 zabbix-agent2 vim
 
 # enable TimescaleDB module
 echo "shared_preload_libraries = 'timescaledb'" >> /var/lib/pgsql/12/data/postgresql.conf
+echo "listen_addresses = '10.133.253.44'" >> /var/lib/pgsql/12/data/postgresql.conf
 
 systemctl enable postgresql-12
 systemctl start postgresql-12
 systemctl status postgresql-12
 
+# check if postgres is listening on IP address
+ss --tcp --listen --numeric
+
+# psql -h'10.133.253.44' --user=zabbix --port=5432
 
 # navigate to posgres user (root for db engine)
 su - postgres
@@ -75,7 +80,7 @@ createdb -O zabbix zabbix
 
 # insert 5.0 schema
 cd ~/zabbix-5.0.4/database/postgresql/
-cat schema.sql | psql --port=5432 zabbix
+cat schema.sql | psql --port=5432 --user=zabbix zabbix
 
 # create timescaledb extenison
 echo "CREATE EXTENSION IF NOT EXISTS timescaledb CASCADE;" | psql zabbix
@@ -95,7 +100,7 @@ echo "SELECT * FROM chunk_relation_size_pretty('trends');" | psql zabbix
 # it should empty table structure only. no hypertables are made yet
 
 
-# poll backup data from old server. without history
+# poll out data from old server. ignore historical data. ignore all hypertables comming from timescaledb
 cd
 PGHOST=10.133.112.87 PGPORT=7412 PGPASSWORD=zabbix PGUSER=postgres \
 pg_dump \
@@ -104,10 +109,70 @@ pg_dump \
 --exclude-table-data=history* \
 --exclude-table-data=trends* \
 --format=plain \
-z50 | gzip > data.sql.gz
+z50 | gzip --fast > /tmp/data.sql.gz
+
+# this will print a message per tables 'triggers', 'items', 'httptest', 'hosts', 'group_prototype', 'graphs', 'application_prototype', 'chunk', 'hypertable':
+# pg_dump: warning: there are circular foreign-key constraints on this table:
+# pg_dump:   httptest
+# pg_dump: You might not be able to restore the dump without using --disable-triggers or temporarily dropping the constraints.
+# pg_dump: Consider using a full dump instead of a --data-only dump to avoid this problem.
+
 
 # restore data. dump does not contain hypertables
-zcat data.sql.gz | psql zabbix
+zcat /tmp/data.sql.gz | psql zabbix
+
+# ERROR:  duplicate key value violates unique constraint "hypertable_pkey"
+# DETAIL:  Key (id)=(1) already exists.
+# CONTEXT:  COPY hypertable, line 1
+# COPY 544
+# ERROR:  duplicate key value violates unique constraint "dimension_pkey"
+# DETAIL:  Key (id)=(1) already exists.
+# CONTEXT:  COPY dimension, line 1
+
+
+
+PGHOST=10.133.112.87 PGPORT=7412 PGPASSWORD=zabbix PGUSER=postgres \
+pg_dump \
+--exclude-schema=_timescaledb_internal \
+--exclude-table-data=history* \
+--exclude-table-data=trends* \
+--exclude-table-data='*hypertable*' \
+--exclude-table-data='*chunk*' \
+--format=plain \
+z50 | gzip --fast > /tmp/all.sql.gz
+
+
+
+PGHOST=10.133.112.87 PGPORT=7412 PGPASSWORD=zabbix PGUSER=postgres \
+pg_dump \
+--data-only \
+--exclude-schema=_timescaledb_internal \
+--exclude-table='*history*' \
+--exclude-table='*trends*' \
+--format=plain \
+z50 | gzip --fast > /tmp/data.sql.gz
+
+# dump individual table
+PGHOST=10.133.112.87 PGPORT=7412 PGPASSWORD=zabbix PGUSER=postgres \
+pg_dump \
+--data-only \
+--table=history \
+--format=plain \
+z50 | gzip --fast > /tmp/history.sql.gz
+
+--exclude-table=table
+
+
+# restore data. dump does not contain hypertables
+zcat /tmp/data.sql.gz | psql zabbix
+
+
+echo "host    all             all             10.133.253.43/32            md5" | sude tee -a /var/lib/pgsql/12/data/pg_hba.conf
+
+systemctl restart postgresql-12
+
+vi /var/lib/pgsql/12/data/pg_hba.conf
+
 
 
 # https://docs.timescale.com/latest/using-timescaledb/backup
