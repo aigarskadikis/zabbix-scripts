06484a6d8dffb76180a1b0422c507cd22e21e882
catonrug <aigars.kadikis@gmail.com>
2022-08-06 22:20:16 +0300

    next

--- CentOS/modular/server-install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#install zabbix server which are supposed to use MySQL as a database
-yum install zabbix-server-mysql-$1 -y
-
-#create zabbix database structure
-ls -l /usr/share/doc/zabbix-server-mysql*/
-zcat /usr/share/doc/zabbix-server-mysql*/create.sql.gz | mysql -uzabbix -pTaL2gPU5U9FcCU2u zabbix
--- CentOS/modular/webserver-install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#enable rhel-7-server-optional-rpms repository. This is neccessary to successfully install frontend
-yum install yum-utils -y
-yum-config-manager --enable rhel-7-server-optional-rpms
-
-#install zabbix frontend
-yum install httpd -y
-yum install zabbix-web-mysql-$1 -y
